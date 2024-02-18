68f44ff4a2f5a252b50f5365799df987f72d3d00
Hanatomizu <chart11from21@outlook.com>
2024-02-18 13:33:55 +0800

    add blocks translations

--- src/main/java/com/awsdkl/periodictableofelements/DataGeneratorProviders/EnglishTranslation.java
+++ src/main/java/com/awsdkl/periodictableofelements/DataGeneratorProviders/EnglishTranslation.java
@@ -12,6 +12,7 @@ public class EnglishTranslation extends FabricLanguageProvider {
 
     @Override
     public void generateTranslations(TranslationBuilder translationBuilder) {
+        // Blocks
         // ELECTROLYZER_MACHINE
         translationBuilder.add(ModBlocks.ELECTROLYZER_MACHINE, "Electrolyzer Machine");
         translationBuilder.add(ModItems.ELECTROLYZER_MACHINE_ITEM, "Electrolyzer Machine");
@@ -28,5 +29,30 @@ public class EnglishTranslation extends FabricLanguageProvider {
         translationBuilder.add(ModBlocks.COPPER_CABLE, "Copper Cable");
         translationBuilder.add(ModItems.COPPER_CABLE_ITEM, "Copper Cable");
         // Tin Ore
+        translationBuilder.add(ModBlocks.TIN_ORE, "Tin Ore");
+        translationBuilder.add(ModItems.TIN_ORE_ITEM, "Tin Ore");
+        // Deepslate Tin Ore
+        translationBuilder.add(ModBlocks.DEEPSLATE_TIN_ORE, "Deepslate Tin Ore");
+        translationBuilder.add(ModItems.DEEPSLATE_TIN_ORE_ITEM, "Deepslate Tin Ore");
+        // Rubber Log
+        translationBuilder.add(ModBlocks.RUBBER_LOG, "Rubber Log");
+        translationBuilder.add(ModItems.RUBBER_LOG_ITEM, "Rubber Log");
+        // Rubber Leaves
+        translationBuilder.add(ModBlocks.RUBBER_LEAVES, "Rubber Leaves");
+        translationBuilder.add(ModItems.RUBBER_LEAVES_ITEM, "Rubber Leaves");
+        // Rubber Wood
+        translationBuilder.add(ModBlocks.RUBBER_WOOD, "Rubber Wood");
+        translationBuilder.add(ModItems.RUBBER_WOOD_ITEM, "Rubber Wood");
+        // Rubber Plank
+        translationBuilder.add(ModBlocks.RUBBER_PLANKS, "Rubber Planks");
+        translationBuilder.add(ModItems.RUBBER_PLANKS_ITEM, "Rubber Planks");
+        // Stripped Rubber Log
+        translationBuilder.add(ModBlocks.STRIPPED_RUBBER_LOG, "Stripped Rubber Log");
+        translationBuilder.add(ModItems.STRIPPED_RUBBER_LOG_ITEM, "Stripped Rubber Log");
+        // Stripped Rubber Wood
+        translationBuilder.add(ModBlocks.STRIPPED_RUBBER_WOOD, "Stripped Rubber Wood");
+        translationBuilder.add(ModItems.STRIPPED_RUBBER_WOOD_ITEM, "Stripped Rubber Wood");
+
+
     }
 }
